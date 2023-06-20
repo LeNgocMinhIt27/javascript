3b27f25f694291070b9684e193898a85633f0cf0
LeNgocMinhIt27 <ngocminhit27@gmail.com>
2023-06-21 01:43:16 +0700

    Tuan2-buoi3

---
+++ tuan2-buoi3/event/main.js
@@ -0,0 +1,21 @@
+function mDown(obj) {
+  obj.style.backgroundColor = "#1ec5e5";
+  obj.innerHTML = "Release Me";
+}
+
+function mUp(obj) {
+  obj.style.backgroundColor = "#D94A38";
+  obj.innerHTML = "Thank You";
+}
+
+function mOver(obj) {
+  obj.innerHTML = "Thank You";
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Mouse Over Me";
+}
+
+function changeText(id) {
+  id.innerHTML = "Ooops!";
+}
