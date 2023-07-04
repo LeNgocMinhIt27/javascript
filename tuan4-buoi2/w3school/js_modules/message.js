014e924550ebe9b6a7a43724f77d2041bbbfb826
LeNgocMinhIt27 <ngocminhit27@gmail.com>
2023-07-05 02:55:50 +0700

    Tuan4-buoi2

---
+++ tuan4-buoi2/w3school/js_modules/message.js
@@ -0,0 +1,11 @@
+const message = () => {
+  const name = "Jesse";
+  const age = 40;
+  return name + " is " + age + " years old.";
+};
+export default message;
+export const person = () => {
+  const name = "pro";
+  const age = 20;
+  return name + " is " + age + " years old";
+};
